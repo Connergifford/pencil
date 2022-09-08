32b5cadfe1f2b93af488d0f07bee85e2969c5769
Connergifford <111781584+Connergifford@users.noreply.github.com>
2022-09-08 12:18:45 -0400

    Add files via upload

---
+++ pencil.py
@@ -0,0 +1,111 @@
+import os
+my_string = ''
+print(my_string)
+line3 = "__________"
+line4 =  "     ^"
+line5 =  "    /#\\"
+line6 =  "   /###\\"
+line7 =  "  /     \\"
+line8 =  " /       \\"
+line9 =  "/~^~^~^~^~\\"
+line10 = "|         |"
+line11 = "|         |"
+line12 = "|         |"
+line13 = "|         |"
+line14 = "|         |"
+line15 = "|         |"
+line16 = "|         |"
+line17 = "|  No*    |"
+line18 = "|         |"
+line19 = "|  ****   |"
+line20 = "|**   **  |"
+line21 = "|    **   |"
+line22 = "|   **    |"
+line23 = "|  **     |"
+line24 = "| ******  |"
+line25 = "| ******  |"
+line26 = "|         |"
+line27 = "|         |"
+line28 = "|         |"
+line29 = "|         |"
+line30 = "|         |"
+line31 = "|         |"
+line32 = "| ! ! ! ! |"
+line33 = "| ! ! ! ! |"
+line34 = "@@@@@@@@@@@"
+line35 = "@@@@@@@@@@@"
+line36 = "@@@@@@@@@@@"
+line37 = " @@@@@@@@@"
+    
+for i in range(20):
+    os.system('CLS')
+    line3 = "_" + line3
+    print(line3)
+    line4 = " " + line4
+    print(line4)
+    line5 = " " + line5
+    print(line5)
+    line6 = " " + line6
+    print(line6)
+    line7 = " " + line7
+    print(line7)
+    line8 = " " + line8
+    print(line8)
+    line9 = " " + line9
+    print(line9)
+    line10 = " " + line10
+    print(line10)
+    line11 = " " + line11
+    print(line11)
+    line12 = " " + line12
+    print(line12)
+    line13 = " " + line13
+    print(line13)
+    line14 = " " + line14
+    print(line14)
+    line15 = " " + line15
+    print(line15)
+    line16 = " " + line16
+    print(line16)
+    line17 = " " + line17
+    print(line17)
+    line18 = " " + line18
+    print(line18)
+    line19 = " " + line19
+    print(line19)
+    line20 = " " + line20
+    print(line20)
+    line21 = " " + line21
+    print(line21)
+    line22 = " " + line22
+    print(line22)
+    line23 = " " + line23
+    print(line23)
+    line24 = " " + line24
+    print(line24)
+    line25 = " " + line25
+    print(line25)
+    line26 = " " + line26
+    print(line26)
+    line27 = " " + line27
+    print(line27)
+    line28 = " " + line28
+    print(line28)
+    line29 = " " + line29
+    print(line29)
+    line30 = " " + line30
+    print(line30)
+    line31 = " " + line31
+    print(line31)
+    line32 = " " + line32
+    print(line32)
+    line33 = " " + line33
+    print(line33)
+    line34 = " " + line34
+    print(line34)
+    line35 = " " + line35
+    print(line35)
+    line36 = " " + line36
+    print(line36)
+    line37 = " " + line37
+    print(line37)
